41dc1d68a6fd65e3d794b6c6e8485b9d8643c1ee
Pedro Henrique <pedro.henrique.a.s66@gmail.com>
2024-05-14 16:25:37 -0300

    feat: implementing main class

--- src/main/java/com/literalura/LiterAlura/LiterAluraApplication.java
+++ src/main/java/com/literalura/LiterAlura/LiterAluraApplication.java
@@ -1,13 +1,24 @@
 package com.literalura.LiterAlura;
 
+import com.literalura.LiterAlura.service.BookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LiterAluraApplication {
+public class LiterAluraApplication implements CommandLineRunner {
+	@Autowired
+	private BookService bookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(LiterAluraApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal(bookService);
+
+		principal.execute();
+	}
 }
